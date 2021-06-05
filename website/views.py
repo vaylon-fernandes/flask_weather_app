7e8947c3a385a4fd5599c2360329f32a3d137a5a
vaylon-fernandes <vaylonfernandes@gmail.com>
2021-06-05 23:21:59 +0530

    Added views to get city name

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from os import environ
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, flash
 from flask.blueprints import Blueprint
+from flask.scaffold import F
 import requests
+from .forms import CityForm
+
 
 views = Blueprint('views', __name__)
 
@@ -11,7 +14,7 @@ API_KEY = environ.get("API_KEY")
 def required_data(data_from_source):
     lon = str(data_from_source['coord']['lon'])
     lat = str(data_from_source['coord']['lat'])
-    coordinates = str(lat + "," + lon)
+    coordinates = str(lat + " ," + lon)
     description = str(data_from_source['weather'][0]['description'])
     icon = str(data_from_source['weather'][0]['icon'])
     temp = str(data_from_source['main']['temp'])
@@ -43,18 +46,35 @@ def required_data(data_from_source):
     return data
 
 
+def form_is_empmty(form):
+    result = form == ''
+    return result
+
+
 @views.route('/', methods=['GET', 'POST'])
-def weather():
-    if request.method == "POST":
-        city = request.form['city']
+@views.route('/<city_name>', methods=['GET', 'POST'])
+@views.route('/<city_name>/<country_name>', methods=['GET', 'POST'])
+def weather(city_name=None, country_name=None):
+    form = CityForm(request.form)
+    if city_name and country_name:
+        city = f"{city_name},{country_name}"
+    elif city_name:
+        city = city_name
     else:
-        city = "Madgaon"
+        if request.method == "POST":
+
+            city = request.form['city']
+            if form_is_empmty(city) and not form.validate():
+                flash('Please enter a City name')
+        else:
+            city = "Madgaon"
 
     url = f"http://api.openweathermap.org/data/2.5/find?q={city}&units=metric&appid={API_KEY}"
 
     get_data = requests.get(url).json()
     cities = get_data["list"]
     count = get_data["count"]
+
     if cities:
         data_to_client = []
         for city_data in cities:
@@ -62,4 +82,12 @@ def weather():
     else:
         data_to_client = "City Not Found"
 
-    return render_template('index.html', data=data_to_client, count=count, city=city)
+    if count > 1:
+
+        flash(
+            f'''Found {count} cities with the name {cities[0]['name']}. 
+            For your search to be more accurate enter  
+            'cityname, two character countrycode'. 
+            For Example, ''', category="Info")
+
+    return render_template('index.html', data=data_to_client, count=count, city=city, country=cities[0]["sys"]["country"])
