5c927059e23157802d69aecc072cdcb609d393d4
vaylon-fernandes <vaylonfernandes@gmail.com>
2021-06-06 23:11:49 +0530

    get secret key using os.environ method

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 import os
-from dotenv import dotenv_values
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = dotenv_values(".env")['SECRET_KEY']
+    app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
     from .views import views
     app.register_blueprint(views, url_prefix="/")
